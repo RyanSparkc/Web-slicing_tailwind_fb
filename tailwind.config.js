03534a22cd9bfa04c59d336dfb707bd1bebdd6d2
forest079 <bluegx08@gmail.com>
2021-12-29 18:18:17 +0800

    更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'main-span': '72px'
+      },
+      colors: {
+        'fb-bg': '#18191a',
+        'fb-header': '#242526',
+        'fb-input': '#3a3b3c',
+        'fb-popover': '#3e4042',
+        'fb-active': '#323436',
+        'fb-card': '#242526',
+        'fb': '#2e89ff'
+      }
+    },
   },
   variants: {
     extend: {},
